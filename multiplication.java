40d2171341f0d87599d6f5da249115881eaafc45
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-08-23 13:54:37 +0530

    multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first value");
+        a=sc.nextInt();
+        System.out.println("enter the second value");
+        b=sc.nextInt();
+        System.out.println("the multiplication of "+a+" and "+b+" is "+(a+b));
+    }
+}
