7eb754e721f3a18feaf3f0faf4d7d7134896b317
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-08-23 23:44:32 +0530

    ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,15 @@
+class Reverse
+{
+    
+    public static String reverseWord(String str)
+    {
+        
+        String s="";
+       for(int i=str.length()-1;i>=0;i--)
+         s+=str.charAt(i);
+       
+       
+       
+        return s;
+    } 
+}
