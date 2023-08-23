7ce83038c1dd2d5ac48e3379517ccf2374b3cf61
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-08-23 13:52:03 +0530

    addition.java

---
+++ addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        System.out.println("Hi!,This is sindu");
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first value");
+        a=sc.nextInt();
+        System.out.println("enter the second value");
+        b=sc.nextInt();
+        System.out.println("the addition of "+a+" and "+b+" is "+(a+b));
+    }
+}
