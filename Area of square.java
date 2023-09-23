d621d78e0e5c4225856a8d7d4da64f23fdbc7cf3
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-09-23 18:44:25 +0530

    Area of square.java

---
+++ Area of square.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+float area,l,b;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the length");
+l=s.nextInt();
+System.out.println("enter the breadth");
+b=s.nextInt();
+area=l*b;
+System.out.println("the area of square is"+area);
+    }
+}
