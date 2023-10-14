2751798db59d161678ab774c0e2ab6cc22d088b5
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-10-14 11:19:25 +0530

    Linkedlist.java

---
+++ Linkedlist.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Linkedlist{
+    public static void main(String[] args) {
+LinkedList<Integer> list=new LinkedList<Integer>();
+Scanner s=new Scanner(System.in);
+System.out.println("enter the total number of values");
+int n=s.nextInt();
+System.out.println("enter the values");
+for(int i=0;i<n;i++)
+     list.add(s.nextInt());
+System.out.println(list);
+
+    }
+}
