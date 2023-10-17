39444c91c2d32de69b3b6f466960bbb7fdc17f12
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-10-17 19:10:22 +0530

    Add element in last of LinkedList.java

---
+++ Add element in last of LinkedList.java
@@ -0,0 +1,9 @@
+class linkedlist{
+public static void main(String[] args){
+
+LinkedList<Integer> li=new LinkedList<Integer>();
+li.add(1);
+li.add(20);
+System.out.println(li);
+li.addLast(12);
+}} 
