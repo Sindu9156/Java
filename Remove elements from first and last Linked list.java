ed6dc8ba98563a174bf44fd3e09cf82d4acce68e
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-10-18 18:26:32 +0530

    Remove elements from first and last Linked list.java

---
+++ Remove elements from first and last Linked list.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class list{
+public static void main(String args[]) {
+LinkedList<Integer> li=new LinkedList<Integer>();
+li.add(1);
+li.add(20);
+System.out.println(li);
+li.removeFirst();
+System.out.println(li);
+li.removeLast();
+System.out.println(li);
+}} 
+
+
+
