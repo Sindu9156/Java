21d87676916cecc4dc4503029a6ef1b1aea997a4
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-10-27 18:11:34 +0530

    Sum of integers in a digit.java

---
+++ Sum of integers in a digit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        int n, rem, sum=0, s;
+        System.out.println("enter the number");
+        Scanner input=new Scanner(System.in);
+        n=input.nextInt();
+        s=n;
+        while(n>0){
+            rem=n%10;
+            n=n/10;
+            sum=sum+rem;
+        }
+        System.out.println("the sum of "+s+" is "+sum);
+    }
+}
