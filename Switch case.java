b0a9dffb160493438f6338520452ef31a368f3e0
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-11-06 22:55:56 +0530

    Switch case.java

---
+++ Switch case.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        int i;
+        Scanner input=new Scanner(System.in);
+        i=input.nextInt();
+        switch(i){
+            case 1:System.out.println("january");
+                   break;
+            case 2:System.out.println("February");
+                   break;
+            case 3:System.out.println("march");
+                   break;
+            case 4:System.out.println("april");
+                   break;
+            case 5:System.out.println("may");
+                   break;
+            case 6:System.out.println("june");
+                   break;
+            case 7:System.out.println("july");
+                   break;
+            case 8:System.out.println("august");
+                   break;
+            case 9:System.out.println("September ");
+                   break;
+            case 10:System.out.println("october");
+                   break;
+            case 11:System.out.println("november");
+                   break;
+            case 12:System.out.println("december");
+                   break;
+      
+        }
+    }
+}
