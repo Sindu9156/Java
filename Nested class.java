b294799c264ffd47be98eef57b709e115b8806ee
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-11-29 20:26:24 +0530

    Nested class.java

---
+++ Nested class.java
@@ -0,0 +1,15 @@
+class Main{
+  Main(){
+        System.out.println("main class");
+    } 
+    class main2{
+       main2(){
+        System.out.println("main2 class");
+       } 
+    }
+    
+    public static void main(String args[]){
+        Main s=new Main();
+        
+    }
+} 
