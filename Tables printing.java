e1953818fc95f29ffae43573e8a6d4f5dd8231f2
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2023-12-01 20:16:23 +0530

    Tables printing.java

---
+++ Tables printing.java
@@ -0,0 +1,10 @@
+class Main{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+System.out.println("enter the value");
+int n=s.nextInt();
+for(int i=1;i<=10;i++)
+     System.out.println(i+"*"+n+"="+(n*i));
+} 
+
+} 
