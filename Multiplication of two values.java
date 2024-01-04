8483f4e2619e61fa94e57476c1a63711831bf37f
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-01-04 18:45:17 +0530

    Multiplication of two values.java

---
+++ Multiplication of two values.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+public static void main(String[] args){
+int a,b;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the two values");
+a=s.nextInt();
+b=s.nextInt();
+System.out.println("multiplication value is" +a*b) ;
+}} 
