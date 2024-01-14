a141078a4c1cfe3a1878035e23dd0472977d166e
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-01-14 20:14:59 +0530

    Table.java

---
+++ Table.java
@@ -0,0 +1,12 @@
+}
+import java.util.*;
+class Main{
+public static void main(String[] args){
+int a,b;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the two values");
+a=s.nextInt();
+b=s.nextInt();
+for(int i=1;i<=b;i++)
+System.out.println(a+" *" +i+" = " +a*i) ;
+}} 
