99d6484c15adf229fe8b437a919c130876fa80ae
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-01-15 20:09:59 +0530

    Print numbers between limit.java

---
+++ Print numbers between limit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+public static void main(String[] args){
+int a,b;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the two values");
+a=s.nextInt();
+b=s.nextInt();
+for(int i=a;i<=b;i++)
+System.out.println(i) ;
+}} 
