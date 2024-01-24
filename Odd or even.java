e28387fd844a148abd91f00a23fb6d917fae8cf8
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-01-24 20:45:44 +0530

    Odd or even.java

---
+++ Odd or even.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        System.out.println("enter the number");
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        if(n%2==0)
+            System.out.println("even");
+        else 
+            System.out.println("even");
+}
+} 
