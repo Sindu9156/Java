00676160c58291132ed819e6825cc260a33f76ff
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-02-06 20:52:20 +0530

    Multiple of a number.java

---
+++ Multiple of a number.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+public static void main(String[] args){
+int a,b;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the value");
+a=s.nextInt();
+for(int i=1;i<=10;i++)
+System.out.println(i*a) ;
+}} 
