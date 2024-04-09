a2cafca76cc93c3b3812fe6f5f66e289de730065
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-04-09 21:12:17 +0530

    Reverse the array.java

---
+++ Reverse the array.java
@@ -0,0 +1,15 @@
+public class Main
+{
+public static void main(String args[])
+{
+Scanner input=new Scanner(System.in);
+System.out.println("enter the total number of values");
+int n=input.nextInt();
+int arr[] =new int[n];
+for(int i=0;i<1;i++)
+    a[i]=input.nextInt();
+int n=a.length;
+for(int i=n-1; i>=0; i--)
+System.out.println(a[i]); 
+}
+}
