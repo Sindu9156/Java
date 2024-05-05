a4fc64614ea165abd5c73e9146075aa100ea6e1e
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-05-05 21:14:54 +0530

    Armstrong number .java

---
+++ Armstrong number .java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    int len;
+Scanner input =new Scanner (System.in);
+System.out.println("enter the total value");
+    int num=input.nextInt();
+      len = order (num);
+
+    
+    if (armstrong (num, len))
+        System.out.println(num + " is armstrong");
+    else
+        System.out.println(num + " is armstrong");
+
+  }
+
+
+  static int order (int x)
+  {
+int len = 0;
+    while (x != 0 )
+      {
+len++;
+x = x / 10;
+      }
+    return len;
+  }
+
+  static boolean armstrong (int num, int len)
+  {
+
+    int sum = 0, temp, digit;
+    temp = num;
+    while (temp != 0)
+      {
+
+digit = temp % 10;
+sum = sum + (int)Math.pow(digit, len);
+temp /= 10;
+      };
+
+    return num == sum;
+  }
+} 
