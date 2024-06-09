2117b59d57fc2ec1b23699f8a2ee0571e1b9565a
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-06-09 21:24:41 +0530

    Pyramid pattern.java

---
+++ Pyramid pattern.java
@@ -0,0 +1,15 @@
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner input=new Scanner(System.in) ;
+    System.out.println("enter the row value");
+    int r=input.nextInt();
+
+    for (int i = 1; i <= r; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
