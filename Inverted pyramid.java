51cf608807a4034065c1812807745d9a2843ca98
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-06-11 22:17:23 +0530

    Inverted pyramid.java

---
+++ Inverted pyramid.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+
+  public static void main(String[] args) {
+      Scanner input=new Scanner(System.in) ;
+     int r=input.nextInt() ;
+
+    for (int i = r; i >= 1; --i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
