1a18cd43861bb46ac6daa1c4fe6dab81b3bb2b51
Sinduja B <127333575+Sindu9156@users.noreply.github.com>
2024-06-29 20:30:03 +0530

    Compile time polymorphism.java

---
+++ Compile time polymorphism.java
@@ -0,0 +1,11 @@
+class Main{  
+static int add(int a, int b){
+return a+b;
+}  
+static int add(int a, int b, int c){
+return a+b+c;
+} 
+public static void main(String[] args){  
+System.out.println(add(11,11));  
+System.out.println(add(12, 2,6));  
+}}  
